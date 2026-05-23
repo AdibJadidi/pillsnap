a07c3d27a2b0587b7ef1247cfcb45a2cc63affab
adoool <adibjadidi74@gmail.com>
2026-05-23 16:18:05 +0330

    feat: add share button with Base64 encoding to UserForm

--- src/features/pill/components/UserForm.tsx
+++ src/features/pill/components/UserForm.tsx
@@ -3,10 +3,33 @@ import React from "react";
 import { usePillStore } from "../store/usePillStore";
 
 const UserForm = () => {
-  const { updateFamilyName, updatePaitentName } = usePillStore();
+  const { updateFamilyName, updatePaitentName, familySchedule } =
+    usePillStore();
+  const handleShare = () => {
+    try {
+      const jsonString = JSON.stringify(familySchedule);
+      const base64Data = btoa(encodeURIComponent(jsonString));
+
+      const shareUrl = `${window.location.origin}/view?data=${base64Data}`;
+
+      navigator.clipboard.writeText(shareUrl);
+      alert("Link generated successfully");
+    } catch (e) {
+      console.error("error", e);
+    }
+  };
   return (
     <div className="flex flex-col gap-y-4 p-6">
-      <h2 className="text-zinc-100">User Information</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-zinc-100 text-sm font-bold">User Information</h2>
+        <button
+          onClick={handleShare}
+          className="bg-sky-600 hover:bg-sky-500 text-white text-xs font-semibold px-4 py-2 rounded-xl transition-all active:scale-95 cursor-pointer"
+        >
+          Share with Patient 🔗
+        </button>
+      </div>
+
       <div className="grid grid-cols-3 gap-4 bg-[#091535]/60 backdrop-blur-md rounded-lg p-4">
         <div className=" flex-1 flex flex-col gap-2 text-zinc-200">
           <label htmlFor="name" className="text-xs">
